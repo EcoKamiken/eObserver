0fc3c6df74ca87ec57c2a399be1a72ff8e3b7f13
Kazuki Nakajima <nakajima.k0301@gmail.com>
2018-10-25 18:00:04 +0900

    update

--- public/parts/date_picker.php
+++ public/parts/date_picker.php
@@ -2,7 +2,7 @@
   <form action="#" method="post">
     <div class="input_area">
       <input name="date" type="text" value="<?php echo $today; ?>" class="datepicker" placeholder="日付を選択">
-      <button type="submit">&#x21BA;</button>
+      <button class="sub_btn" type="submit"><span>&#x21BA;</span></button>
     </div>
   </form>
 </div>

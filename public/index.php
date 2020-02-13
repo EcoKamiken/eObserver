12635238b1d15de62e25d1bad727b0290fd2e266
Kazuki Nakajima <nakajima.k0301@gmail.com>
2020-02-13 16:07:35 +0900

    最低限動く状態に戻した

--- public/index.php
+++ public/index.php
@@ -16,24 +16,23 @@ require('parts/datepicker.php');
 
 <?php
 
-$sql =<<<__LONG_STRRING__
+$sql = "
 SELECT
   id, name, capacity, machine_type
 FROM
   sites
 ORDER BY
   grp, serial_number
-__LONG_STRRING__;
+";
 
 $connection = new Database\Database();
 $stmt = $connection->dbh->query($sql);
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$sites = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 
 <!-- foreach begin -->
 <?php
-foreach ($rows as $row) {
+foreach ($sites as $row) {
     ?>
 
     <a href="detail.php?id=<?php echo $row['id']; ?>&today=<?php echo $date->begin_date; ?>">
@@ -53,47 +52,32 @@ foreach ($rows as $row) {
 require('parts/footer.php');
 
 // データを取得して、mainの中で生成したキャンバスにグラフを描画する。
-/*
-$pdo = get_pdo();
 foreach ($sites as $row) {
     $sql = "
     select
-        date_format(created_at, '%Y-%m-%d %H:00:00') as times,
-        round(sum(temperature)/count(*), 0) as temperature,
-        round(sum(humidity)/count(*), 0) as humidity,
-        round(sum(wattage)/count(*), 2) as wattage
+        date_format(created_at, '%Y-%m-%d %H:00:00') as c_at,
+        round(sum(wattage)/count(*), 2) as w_avg
     from
         sensors
     where
-        created_at between :today and :tommorow
+        created_at between :begin_date and :end_date
         and id = :id
-        and device_id = :device_id
     group by
-        times
+        c_at
     ";
 
-    foreach(range(0, $row['device_qty'] - 1) as $device_id) {
-      $id = $row['id'];
-      $name = $row['name'] . " " . $row['capacity'] . "kW";
+    $stmt = $connection->dbh->prepare($sql);
+    $stmt->bindValue(':begin_date', $date->begin_date.' 00:00:00', PDO::PARAM_STR);
+    $stmt->bindValue(':end_date', $date->end_date.' 00:00:00', PDO::PARAM_STR);
+    $stmt->bindValue(':id', (int)$row['id'], PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-      $stmt = $pdo->prepare($sql);
-      $stmt->bindValue(':today', $today.' 00:00:00', PDO::PARAM_STR);
-      $stmt->bindValue(':tommorow', $tommorow.' 00:00:00', PDO::PARAM_STR);
-      $stmt->bindValue(':id', (int)$row['id'], PDO::PARAM_INT);
-      $stmt->bindValue(':device_id', $device_id, PDO::PARAM_INT);
-      $stmt->execute();
-      if(!$result) {
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      } else {
-        $tmp = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        for($i = 0; $i < count($result); $i++) {
-          $result[$i]['wattage'] += $tmp[$i]['wattage'];
-        }
-      }
-    }
+    $name = $row['name'] . " " . $row['capacity'] . "kW";
+    $id = $row['id'];
+
     $json = json_safe_encode($result);
     $result = null;
     echo "\n<script>drawGraph('$name', '$id', '$json', false);</script>";
 }
-*/
 ?>
